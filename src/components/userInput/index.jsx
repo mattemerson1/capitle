d0219e75dd8e4a2988a71c5635ca6a043987390a
matt emerson <matthew.emerson@ibm.com>
2022-03-27 22:39:23 +0100

    test new autocomplete input dropdown

--- src/components/userInput/index.jsx
+++ src/components/userInput/index.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { Autocomplete, TextField } from '@mui/material';
+
+const capitalCities = ['London', 'Beijing', 'Mexico City', 'Canberra', 'Gitega']
 
 const UserInput = (props) => {
   const [userGuess, setUserGuess] = useState("");
@@ -6,6 +9,7 @@ const UserInput = (props) => {
   const [isGameWon, setIsGameWon] = useState(false);
   
   const addGuess = (userGuess, e) => {
+    console.log("add GUESS", userGuess, e.target.value);
     const formatUserGuess = userGuess.charAt(0).toUpperCase() + userGuess.slice(1).toLowerCase();
     props.addGuess(formatUserGuess, e);
     setUserGuess("");
@@ -26,16 +30,30 @@ const UserInput = (props) => {
   }
 
   return (
-    <form onSubmit={(e) => addGuess(userGuess, e)}>
-      <label>Enter capital city:
-        <input
-          type="text"
-          disabled={isGameWon || isEndGame}
-          value={userGuess}
-          onChange={(e) => setUserGuess(e.target.value)}
-        />
-      </label>
-    </form>
+    // <form onSubmit={(e) => addGuess(userGuess, e)}>
+    //   <label>Enter capital city:
+    //     <input
+    //       type="text"
+    //       disabled={isGameWon || isEndGame}
+    //       value={userGuess}
+    //       onChange={(e) => setUserGuess(e.target.value)}
+    //     />
+    //   </label>
+    // </form>
+    <Autocomplete
+    disablePortal
+    id="combo-box-demo"
+    options={capitalCities}
+    sx={{ width: 300 }}
+    value={userGuess}
+    onChange={(e, userGuess) => addGuess(userGuess, e) && e.preventDefault()}
+    disabled={isGameWon || isEndGame}
+    renderInput={(params) => <TextField 
+      {...params}  
+      label="Enter capitial city:" 
+
+    />}
+  />
   );
 };
 
