1e487cf4c31dfb2e32fc9df9da3fba60fb1d28d9
matt emerson <matthew.emerson@ibm.com>
2022-04-29 19:08:04 +0100

    Help icon and modal

--- src/components/appBar/index.jsx
+++ src/components/appBar/index.jsx
@@ -3,18 +3,28 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import MenuIcon from '@mui/icons-material/Menu';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
 import IconButton from '@mui/material/IconButton';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const EndGame = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
 
+  const handleOpenModal = () => setOpen(true);
+  const handleCloseModal = () => setOpen(false);
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   return (
     <Box>
       <AppBar style={{ position: 'relative', background: '#2E3B55' }}>
@@ -28,28 +38,30 @@ const EndGame = () => {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={handleOpenModal}
           >
-            <MenuIcon />
+            <HelpOutlineOutlinedIcon />
           </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorEl}
-            getContentAnchorEl={null}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            // keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
+          <Modal
+            open={open}
+            onBackdropClick={handleCloseModal}
+            // onClose={handleCloseModal}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
           >
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem onClick={handleClose}>My account</MenuItem>
-          </Menu>
+            <Box sx={style} >
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                How to play capitle
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Your aim is to guess the randomly selected capital city using the fewest number of guesses.
+                For each guess you will be able to see your proximity to the answer based off any matches in the 5 distict categories: Area, Continent, GDP, Population, and Avg Temp.
+                For categories which contain continious data sets the they are ranked into 4 levels.
+                For example: Population is split into 4 levels, capitials over 5m population - rank 1, captials 1m - 5m population - rank 2, capitals 100k - 1m population - rank 3, and capitals under 100k - rank4.
+                If you guess a capital in the correct rank the relative square will be green, if it is incorrect the relative square will be red
+              </Typography>
+            </Box>
+          </Modal>
         </Toolbar>
       </AppBar>
     </Box>
